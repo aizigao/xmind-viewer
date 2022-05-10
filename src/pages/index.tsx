00fa41ee2f53dd5901a12d09b3bd42e84e271de5
aizigao <weixiangzhe@live.com>
2022-05-10 16:51:11 +0800

    feat:init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './index.less';
+import main from './main';
+import logo from '@/assets/icon.png';
 
 export default function () {
+  useEffect(() => {
+    main();
+  }, []);
   return (
     <div>
       <header>
         <div id="logo">
-          <img width="32px" height="32px" src="@/assets/icon.png" />
+          <img width="32px" height="32px" src={logo} />
         </div>
         <button type="button" id="open-file">
           Open File
         </button>
-        <input id="input-dialog" type="file" style="display: none" />
+        <input id="input-dialog" type="file" style={{ display: 'none' }} />
       </header>
       <div id="page-content"></div>
     </div>
